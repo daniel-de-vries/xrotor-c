2eb8d906cc4f47b31e7422e5a8ed71ce2182ad96
Daniel de Vries <daniel.devries@darcorp.com>
2018-11-30 16:07:04 -0600

    Added placeholder headers for missing components.

---
+++ include/xbend.h
@@ -0,0 +1,18 @@
+//
+// Created by daniel.devries on 11/30/2018.
+//
+
+#ifndef XROTOR_NOGRAPHICS_C_XBEND_H
+#define XROTOR_NOGRAPHICS_C_XBEND_H
+
+#include "common.h"
+#include <iostream>
+using namespace std;
+
+namespace xbend {
+    void bend(common::context& context) {
+        cout << "NOT IMPLEMENTED" << endl;
+    }
+}
+
+#endif //XROTOR_NOGRAPHICS_C_XBEND_H
---
+++ include/xnoise.h
@@ -0,0 +1,18 @@
+//
+// Created by daniel.devries on 11/30/2018.
+//
+
+#ifndef XROTOR_NOGRAPHICS_C_XNOISE_H
+#define XROTOR_NOGRAPHICS_C_XNOISE_H
+
+#include "common.h"
+#include <iostream>
+using namespace std;
+
+namespace xnoise {
+    void noise(common::context& context) {
+        cout << "NOT IMPLEMENTED" << endl;
+    }
+}
+
+#endif //XROTOR_NOGRAPHICS_C_XNOISE_H

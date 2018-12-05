4c5f81c76f19a95dfb1083dd7aa030f762353c47
Daniel de Vries <daniel.devries@darcorp.com>
2018-12-05 09:33:39 -0600

    Improved layout of common.h

--- include/common.h
+++ include/common.h
@@ -12,14 +12,14 @@
 using namespace std;
 
 namespace common {
-    const int IX = 100;                 /**< max number of radial prop stations */
+    const int IX = 100;                     /**< max number of radial prop stations */
     const int IPX = IX + 1;
-    const int NPARX = 12;               /**< max number of stored cases */
-    const int ICASX = 100;              /**< number of case parameters stored */
-    const int IWX = 200;                /**< dimension of work arrays */
+    const int NPARX = 12;                   /**< max number of stored cases */
+    const int ICASX = 100;                  /**< number of case parameters stored */
+    const int IWX = 200;                    /**< dimension of work arrays */
 
-    const int NAX = 20;                 /**< max number of aerodynamic sections defined */
-    const int NDX = 14;                 /**< number of aerodynamic parameters defined for each section */
+    const int NAX = 20;                     /**< max number of aerodynamic sections defined */
+    const int NDX = 14;                     /**< number of aerodynamic parameters defined for each section */
 
     const int IQ = IX + 5;
     const int JX = (IQ * 3) / 2 + 1;
@@ -27,75 +27,182 @@ namespace common {
     const double PI = 3.14159265358979323846264338327950288;
 
     struct context {
-        double Q[IQ][IQ];
+        double  Q           [IQ][IQ];       /**< working array */
 
-        double MCRIT;
+        double  MCRIT;                      /**< critical Mach number */
 
-        bool CONV, GREEK, TERSE, VRTX, FAST, FREE, DUCT, LSTRUCT,
-            DEST, DESP, LDESINI, LOPRINI,
-            LROTOR, LVNORM, LPWRVAR, STALL[IX], LEGEND;
+        bool    CONV,                       /**< true if Converged solution exists */
+                GREEK,                      /**< true if Unrecognized command */
+                TERSE,                      /**< true if Terse output (no radial distributions) */
+                VRTX,                       /**<  */
+                FAST,                       /**< true if Graded Momentum, Potential otherwise */
+                FREE,                       /**< true if Free wake */
+                DUCT,                       /**< true if duct is present */
+                LSTRUCT,                    /**< */
+                DEST,                       /**< */
+                DESP,                       /**< */
+                LDESINI,                    /**< */
+                LOPRINI,                    /**< */
+                LROTOR,                     /**< */
+                LVNORM,                     /**< */
+                LPWRVAR,                    /**< */
+                STALL       [IX],           /**< */
+                LEGEND;                     /**< */
 
-        string SAVIL, FNAME, NAME;
+        string  SAVIL,                      /**< */
+                FNAME,                      /**< */
+                NAME;                       /**< */
 
-        double RHO, RMU, VSO, VEL, RAD, GEE, ALT;
+        double  RHO,                        /**< */
+                RMU,                        /**< */
+                VSO,                        /**< */
+                VEL,                        /**< */
+                RAD,                        /**< */
+                GEE,                        /**< */
+                ALT;                        /**< */
 
-        int II, IINF, INCR, NN, NBLDS, IXSPAC,
-            NITERD, NITERA;
+        int     II,                         /**< */
+                IINF,                       /**< */
+                INCR,                       /**< */
+                NN,                         /**< */
+                NBLDS,                      /**< */
+                IXSPAC,                     /**< */
+                NITERD,                     /**< */
+                NITERA;                     /**< */
 
-        float VERSION;
-        double DT;
+        float   VERSION;                    /**< */
+        double  DT;                         /**< */
 
-        int IAERO[IX];
+        int     IAERO       [IX];           /**< */
 
-        double CH[IX], BETA[IX], BETA0[IX], T[IX], DBETA,
-               XI[IX], DXI[IX], XI0, XITIP, XINF,
-               XPITCH, XV[IX], RAKE;
+        double  CH          [IX],           /**< */
+                BETA        [IX],           /**< */
+                BETA0       [IX],           /**< */
+                T           [IX],           /**< */
+                DBETA,                      /**< */
+                XI          [IX],           /**< */
+                DXI         [IX],           /**< */
+                XI0,                        /**< */
+                XITIP,                      /**< */
+                XINF,                       /**< */
+                XPITCH,                     /**< */
+                XV          [IX],           /**< */
+                RAKE;                       /**< */
 
-        int NADD;
-        double RADD[IX],
-               UADD[IX], VADD[IX],
-               UADDR[IX], VADDR[IX],
-               UBODY[IX], VBODY[IX], URDUCT;
+        int     NADD;                       /**< */
+        double  RADD        [IX],           /**< */
+                UADD        [IX],           /**< */
+                VADD        [IX],           /**< */
+                UADDR       [IX],           /**< */
+                VADDR       [IX],           /**< */
+                UBODY       [IX],           /**< */
+                VBODY       [IX],           /**< */
+                URDUCT;                     /**< */
 
-        double CL[IX], CD[IX], CM[IX],
-               RE[IX], EFFP[IX], GAM[IX],
-               DTII[IX], DPII[IX],
-               DTVI[IX], DPVI[IX],
-               DTWI[IX], DPWI[IX];
+        double  CL          [IX],           /**< */
+                CD          [IX],           /**< */
+                CM          [IX],           /**< */
+                RE          [IX],           /**< */
+                EFFP        [IX],           /**< */
+                GAM         [IX],           /**< */
+                DTII        [IX],           /**< */
+                DPII        [IX],           /**< */
+                DTVI        [IX],           /**< */
+                DPVI        [IX],           /**< */
+                DTWI        [IX],           /**< */
+                DPWI        [IX];           /**< */
 
-        int NAERO;
-        double XIAERO[NAX], AERODATA[NDX][NAX];
+        int     NAERO;
+        double  XIAERO      [NAX],          /**< */
+                AERODATA    [NDX][NAX];     /**< */
 
-        int NCASE, KCASE, IWTYP;
+        int     NCASE,                      /**< */
+                KCASE,                      /**< */
+                IWTYP;                      /**< */
 
-        double ADV, ADW, ADWFCTR,
-               RMS, RLX, EFFINV,
-               TSPEC, PSPEC, QSPEC,
-               TTOT, PTOT, QTOT,
-               TINV, PINV, TWAK, PWAK, TVIS, PVIS,
-               GRESMX, FRESMX, ARESMX;
+        double  ADV,                        /**< */
+                ADW,                        /**< */
+                ADWFCTR,                    /**< */
+                RMS,                        /**< */
+                RLX,                        /**< */
+                EFFINV,                     /**< */
+                TSPEC,                      /**< */
+                PSPEC,                      /**< */
+                QSPEC,                      /**< */
+                TTOT,                       /**< */
+                PTOT,                       /**< */
+                QTOT,                       /**< */
+                TINV,                       /**< */
+                PINV,                       /**< */
+                TWAK,                       /**< */
+                PWAK,                       /**< */
+                TVIS,                       /**< */
+                PVIS,                       /**< */
+                GRESMX,                     /**< */
+                FRESMX,                     /**< */
+                ARESMX;                     /**< */
 
-        double TI_ADV,PI_ADV,TI_ADW,PI_ADW,
-               TW_ADV,PW_ADV,TW_ADW,PW_ADW,
-               TV_ADV,PV_ADV,TV_ADW,PV_ADW,TV_DBE,PV_DBE,
-               TI_GAM[IX],PI_GAM[IX],
-               TW_GAM[IX],PW_GAM[IX],
-               TV_GAM[IX],PV_GAM[IX];
+        double  TI_ADV,                     /**< */
+                PI_ADV,                     /**< */
+                TI_ADW,                     /**< */
+                PI_ADW,                     /**< */
+                TW_ADV,                     /**< */
+                PW_ADV,                     /**< */
+                TW_ADW,                     /**< */
+                PW_ADW,                     /**< */
+                TV_ADV,                     /**< */
+                PV_ADV,                     /**< */
+                TV_ADW,                     /**< */
+                PV_ADW,                     /**< */
+                TV_DBE,                     /**< */
+                PV_DBE,                     /**< */
+                TI_GAM      [IX],           /**< */
+                PI_GAM      [IX],           /**< */
+                TW_GAM      [IX],           /**< */
+                PW_GAM      [IX],           /**< */
+                TV_GAM      [IX],           /**< */
+                PV_GAM      [IX];           /**< */
 
-        double W0[IWX], W1[IWX], W2[IWX], W3[IWX], W4[IWX],
-               W5[IWX], W6[IWX], W7[IWX], W8[IWX], W9[IWX];
+        double  W0          [IWX],          /**< */
+                W1          [IWX],          /**< */
+                W2          [IWX],          /**< */
+                W3          [IWX],          /**< */
+                W4          [IWX],          /**< */
+                W5          [IWX],          /**< */
+                W6          [IWX],          /**< */
+                W7          [IWX],          /**< */
+                W8          [IWX],          /**< */
+                W9          [IWX];          /**< */
 
-        int NPWRVAR;
+        int     NPWRVAR;                    /**< */
 
-        double VWAK[IX], VW_GAM[IX][IX], VW_ADW[IX], VW_ADV[IX],
-               VIND[3][IX], VIND_GAM[3][IX][IX], VIND_ADW[3][IX];
+        double  VWAK        [IX],           /**< */
+                VW_GAM      [IX][IX],       /**< */
+                VW_ADW      [IX],           /**< */
+                VW_ADV      [IX],           /**< */
+                VIND        [3][IX],        /**< */
+                VIND_GAM    [3][IX][IX],    /**< */
+                VIND_ADW    [3][IX];        /**< */
 
-        double XW0, XWTIP,
-               XW[IX],   xw_gam[IX][IX],  xw_adw[IX][IX],  xw_adv[IX][IX],
-               dwx[IX], dxw_gam[IX][IX], dxw_adw[IX][IX], dxw_adv[IX][IX];
+        double  XW0,
+                XWTIP,
+                XW          [IX],           /**< */
+                XW_GAM      [IX][IX],       /**< */
+                XW_ADW      [IX][IX],       /**< */
+                XW_ADV      [IX][IX],       /**< */
+                DWX         [IX],           /**< */
+                DXW_GAM     [IX][IX],       /**< */
+                DXW_ADW     [IX][IX],       /**< */
+                DXW_ADV     [IX][IX];       /**< */
 
-        double DGAM[IX], RES[IQ], DADV, DADW, DBET, DEFF, DQ[IQ],
-               DGAMOLD[IX];
+        double  DGAM        [IX],           /**< */
+                RES         [IQ],           /**< */
+                DADV,                       /**< */
+                DADW,                       /**< */
+                DBET,                       /**< */
+                DEFF,                       /**< */
+                DQ          [IQ],           /**< */
+                DGAMOLD     [IX];           /**< */
 
     };
 
